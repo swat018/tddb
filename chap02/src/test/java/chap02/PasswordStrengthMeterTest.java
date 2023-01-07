7df0d2a7a8c8a8321c6281f6bd11e3041992572a
jinwoo park <swat018@gmail.com>
2023-01-07 23:29:12 +0900

    fix : Chap02 - TDD 예: 암호 검사기 / 첫 번째 테스트: 모든 규칙을 충족하는 경우 (오타수정)

--- chap02/src/test/java/chap02/PasswordStrengthMeterTest.java
+++ chap02/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PasswordStrengthMeterTest {
 
     @Test
-    void meetsAllCriteria_Then_String() {
+    void meetsAllCriteria_Then_Strong() {
         PasswordStrengthMeter meter = new PasswordStrengthMeter();
         PasswordStrength result = meter.meter("ab12!@AB");
         assertEquals(PasswordStrength.STRONG, result);
